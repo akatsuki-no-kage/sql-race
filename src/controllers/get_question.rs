bce3e684be8788880b97701dcc99d205b0548082
LeviathanST <phamquochung170505@gmail.com>
2024-10-07 22:48:13 +0700

    refactor: change args name

--- src/controllers/get_question.rs
+++ src/controllers/get_question.rs
@@ -4,10 +4,10 @@ use tokio::fs;
 
 use crate::models::question::Question;
 
-pub async fn get_question(path_dir: &Path) -> Result<Question> {
-    let answer = fs::read_to_string(path_dir.join("answer.sql")).await?;
-    let question = fs::read_to_string(path_dir.join("question.txt")).await?;
-    let schema = fs::read_to_string(path_dir.join("schema.sql")).await?;
+pub async fn get_question(quesiton_dir: &Path) -> Result<Question> {
+    let answer = fs::read_to_string(quesiton_dir.join("answer.sql")).await?;
+    let question = fs::read_to_string(quesiton_dir.join("question.txt")).await?;
+    let schema = fs::read_to_string(quesiton_dir.join("schema.sql")).await?;
 
     Ok(Question {
         question,
