0ed5529c8e380f0d400df0d6e5895cdce22561a7
LeviathanST <phamquochung170505@gmail.com>
2024-10-07 22:46:50 +0700

    update: change String to Path

--- src/controllers/get_question.rs
+++ src/controllers/get_question.rs
@@ -1,12 +1,13 @@
 use anyhow::Result;
+use std::path::Path;
 use tokio::fs;
 
 use crate::models::question::Question;
 
-pub async fn get_question(path_dir: String) -> Result<Question> {
-    let answer = fs::read_to_string(format!("{}/answer.sql", path_dir)).await?;
-    let question = fs::read_to_string(format!("{}/question.txt", path_dir)).await?;
-    let schema = fs::read_to_string(format!("{}/schema.sql", path_dir)).await?;
+pub async fn get_question(path_dir: &Path) -> Result<Question> {
+    let answer = fs::read_to_string(path_dir.join("answer.sql")).await?;
+    let question = fs::read_to_string(path_dir.join("question.txt")).await?;
+    let schema = fs::read_to_string(path_dir.join("schema.sql")).await?;
 
     Ok(Question {
         question,
