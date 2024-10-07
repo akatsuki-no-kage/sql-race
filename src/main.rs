4db62c0a5e6ebbe0295938e5066084d62bc373b9
LeviathanST <phamquochung170505@gmail.com>
2024-10-07 22:34:39 +0700

    feat(controllers/get_question): func get a quest

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use anyhow::Result;
+use controllers::get_question;
+
 pub mod controllers;
+pub mod models;
 pub mod views;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<()> {
+    let path = "./questions/qs_1";
+
+    let question_data = get_question(path.to_string()).await?;
+    println!("{:?}", question_data);
+
+    Ok(())
 }
