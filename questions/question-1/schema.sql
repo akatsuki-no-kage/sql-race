f5051d564a0ccb79f53c595ce886bb7568b46817
LeviathanST <phamquochung170505@gmail.com>
2024-11-12 01:19:31 +0700

    update: score and question schema

--- questions/question-1/schema.sql
+++ questions/question-1/schema.sql
@@ -2,5 +2,7 @@ CREATE TABLE IF NOT EXISTS test (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     score INT NOT NULL,
-    created_at TIMESTAMP NOT NULL
-)
+    created_at TIMESTAMP DEFAULT current_timestamp NOT NULL
+);
+
+INSERT INTO test (username, score) VALUES ('hung', 39);
