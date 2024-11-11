0e4306f5e125f029078d71762c98ea60aeac8185
LeviathanST <phamquochung170505@gmail.com>
2024-11-12 03:36:46 +0700

    add: check exist username

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,8 +1,10 @@
+mod check_exist_username;
 mod get_question;
 mod get_score;
 mod run_query;
 mod view_schema;
 
+pub use check_exist_username::*;
 pub use get_question::*;
 pub use get_score::*;
 pub use run_query::*;
