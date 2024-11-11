642d580c4dba8f165193331697d4518868ad8de4
LeviathanST <phamquochung170505@gmail.com>
2024-11-12 03:38:41 +0700

    additional field in app

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,10 @@
+use anyhow::Result;
+use sqlx::SqlitePool;
+
 pub struct App {
+    pub pool: SqlitePool,
+    pub username: String,
+    pub score: i64,
     pub exit: bool,
     pub state: AppState,
 }
@@ -10,10 +16,14 @@ pub enum AppState {
 }
 
 impl App {
-    pub fn new() -> Self {
-        Self {
+    pub async fn new(username: String) -> Result<Self> {
+        let pool = SqlitePool::connect("sqlite:score.db").await?;
+        Ok(Self {
+            pool,
+            username,
+            score: 0,
             exit: false,
-            state: AppState::InGame,
-        }
+            state: AppState::Menu,
+        })
     }
 }
