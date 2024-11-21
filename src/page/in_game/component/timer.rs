7fa86157e973f59937b6d0fff7ad59383273d576
akagiyuu <khanghuynh0245@gmail.com>
2024-11-21 21:26:18 +0700

    feat(in_game/component): add timer

---
+++ src/page/in_game/component/timer.rs
@@ -0,0 +1,22 @@
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    widgets::{Block, Borders, Paragraph, Widget},
+};
+
+use crate::page::in_game::InGameState;
+
+pub struct Timer<'a> {
+    in_game_state: &'a InGameState,
+}
+
+impl Widget for Timer<'_> {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let time_left = self.in_game_state.get_time_left();
+
+        Paragraph::new(time_left.as_secs().to_string())
+            .centered()
+            .block(Block::default().title("Time left").borders(Borders::ALL))
+            .render(area, buf);
+    }
+}
