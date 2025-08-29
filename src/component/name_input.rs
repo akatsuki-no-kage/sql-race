6d5b8ff8bc2a78433a2835ab7646de0a8cdefdc2
akagiyuu <khanghuynh0245@gmail.com>
2025-08-29 17:01:34 +0700

    feat(component/name_input): use tuirealm input

--- src/component/name_input.rs
+++ src/component/name_input.rs
@@ -1,121 +1,65 @@
-use ratatui::style::Style;
+use ratatui::style::{Color, Style};
+use tui_realm_stdlib::Input;
 use tuirealm::{
     Component, Event, MockComponent,
-    command::{Cmd, Direction, Position},
+    command::{Cmd, CmdResult, Direction, Position},
     event::{Key, KeyEvent, KeyModifiers},
-    props::{Alignment, BorderSides, Borders},
+    props::{Alignment, BorderSides, Borders, InputType, TextModifiers},
 };
 
-use crate::{
-    Message,
-    component::textarea::{self, TextArea},
-    event::UserEvent,
-};
+use crate::{Message, event::UserEvent};
 
 #[derive(MockComponent)]
-pub struct NameInput<'a> {
-    component: TextArea<'a>,
+pub struct NameInput {
+    component: Input,
 }
 
-impl Default for NameInput<'_> {
+impl Default for NameInput {
     fn default() -> Self {
         Self {
-            component: TextArea::default()
-                .single_line(true)
+            component: Input::default()
                 .borders(Borders::default().sides(BorderSides::all()))
-                .cursor_line_style(Style::default())
-                .title("Name", Alignment::Center),
+                .title("Name", Alignment::Center)
+                .input_type(InputType::Text)
+                .placeholder(
+                    "Input your name",
+                    Style::new()
+                        .fg(Color::DarkGray)
+                        .add_modifier(TextModifiers::DIM),
+                )
+                .invalid_style(Style::default().fg(Color::Red)),
         }
     }
 }
 
-impl Component<Message, UserEvent> for NameInput<'_> {
+impl Component<Message, UserEvent> for NameInput {
     fn on(&mut self, event: Event<UserEvent>) -> Option<Message> {
-        match event {
+        let _ = match event {
+            Event::Keyboard(KeyEvent {
+                code: Key::Left, ..
+            }) => self.perform(Cmd::Move(Direction::Left)),
+            Event::Keyboard(KeyEvent {
+                code: Key::Right, ..
+            }) => self.perform(Cmd::Move(Direction::Right)),
+            Event::Keyboard(KeyEvent {
+                code: Key::Home, ..
+            }) => self.perform(Cmd::GoTo(Position::Begin)),
+            Event::Keyboard(KeyEvent { code: Key::End, .. }) => {
+                self.perform(Cmd::GoTo(Position::End))
+            }
+            Event::Keyboard(KeyEvent {
+                code: Key::Delete, ..
+            }) => self.perform(Cmd::Cancel),
             Event::Keyboard(KeyEvent {
                 code: Key::Backspace,
                 ..
-            }) => {
-                self.perform(Cmd::Delete);
-                Some(Message::None)
-            }
-            Event::Keyboard(KeyEvent {
-                code: Key::Left,
-                modifiers: KeyModifiers::SHIFT,
-            }) => {
-                self.perform(textarea::command::MOVE_WORD_BACK);
-                Some(Message::None)
-            }
-            Event::Keyboard(KeyEvent {
-                code: Key::Left, ..
-            }) => {
-                self.perform(Cmd::Move(Direction::Left));
-                Some(Message::None)
-            }
-            Event::Keyboard(KeyEvent {
-                code: Key::Right,
-                modifiers: KeyModifiers::SHIFT,
-            }) => {
-                self.perform(textarea::command::MOVE_WORD_FORWARD);
-                Some(Message::None)
-            }
-            Event::Keyboard(KeyEvent {
-                code: Key::Right, ..
-            }) => {
-                self.perform(Cmd::Move(Direction::Right));
-                Some(Message::None)
-            }
-            Event::Keyboard(KeyEvent { code: Key::End, .. })
-            | Event::Keyboard(KeyEvent {
-                code: Key::Char('e'),
-                modifiers: KeyModifiers::CONTROL,
-            }) => {
-                self.perform(Cmd::GoTo(Position::End));
-                Some(Message::None)
-            }
-            Event::Keyboard(KeyEvent {
-                code: Key::Home, ..
-            })
-            | Event::Keyboard(KeyEvent {
-                code: Key::Char('a'),
-                modifiers: KeyModifiers::CONTROL,
-            }) => {
-                self.perform(Cmd::GoTo(Position::Begin));
-                Some(Message::None)
-            }
-            Event::Keyboard(KeyEvent {
-                code: Key::Char('v'),
-                modifiers: KeyModifiers::CONTROL,
-            }) => {
-                self.perform(textarea::command::PASTE);
-                Some(Message::None)
-            }
-            Event::Keyboard(KeyEvent {
-                code: Key::Char('z'),
-                modifiers: KeyModifiers::CONTROL,
-            }) => {
-                self.perform(textarea::command::UNDO);
-                Some(Message::None)
-            }
-            Event::Keyboard(KeyEvent {
-                code: Key::Char('y'),
-                modifiers: KeyModifiers::CONTROL,
-            }) => {
-                self.perform(textarea::command::REDO);
-                Some(Message::None)
-            }
-            Event::Keyboard(KeyEvent { code: Key::Tab, .. }) => {
-                self.perform(Cmd::Type('\t'));
-                Some(Message::None)
-            }
+            }) => self.perform(Cmd::Delete),
             Event::Keyboard(KeyEvent {
                 code: Key::Char(ch),
-                ..
-            }) => {
-                self.perform(Cmd::Type(ch));
-                Some(Message::None)
-            }
-            _ => None,
-        }
+                modifiers: KeyModifiers::NONE,
+            }) => self.perform(Cmd::Type(ch)),
+            _ => CmdResult::None,
+        };
+        Some(Message::None)
     }
 }
