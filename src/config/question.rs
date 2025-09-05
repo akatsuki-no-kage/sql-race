76136049aa383e45849e2d1edde1a0c6ec3fda07
akagiyuu <khanghuynh0245@gmail.com>
2025-09-06 02:00:29 +0700

    feat(config): add question count for each level

--- src/config/question.rs
+++ src/config/question.rs
@@ -1,9 +1,27 @@
-use std::path::PathBuf;
+use std::{collections::HashMap, path::PathBuf};
 
 use serde::Deserialize;
+
+#[derive(Debug, PartialEq, Eq, Hash, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Level {
+    Easy,
+    Medium,
+    Hard,
+}
+
+impl From<Level> for &'static str {
+    fn from(level: Level) -> Self {
+        match level {
+            Level::Easy => "easy",
+            Level::Medium => "medium",
+            Level::Hard => "hard",
+        }
+    }
+}
 
 #[derive(Debug, Deserialize)]
 pub struct QuestionConfig {
     pub root: PathBuf,
-    pub count: usize,
+    pub count: HashMap<Level, usize>,
 }
